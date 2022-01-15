f00663fe9e5724ba5354964627b9b6873e7693af
leepriscilla <lee.priscilla98@gmail.com>
2022-01-15 02:23:48 -0800

    Update front page, about me, contact me

--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -126,7 +126,7 @@ const About = () => {
     sr.reveal(revealContainer.current, srConfig());
   }, []);
 
-  const skills = ['JavaScript (ES6+)', 'TypeScript', 'React', 'Eleventy', 'Node.js', 'WordPress'];
+  const skills = ['Java', 'C++', 'Python', 'Javascript', 'HTML', 'CSS'];
 
   return (
     <StyledAboutSection id="about" ref={revealContainer}>
@@ -136,30 +136,17 @@ const About = () => {
         <StyledText>
           <div>
             <p>
-              Hello! My name is Brittany and I enjoy creating things that live on the internet. My
-              interest in web development started back in 2012 when I decided to try editing custom
-              Tumblr themes — turns out hacking together a custom reblog button taught me a lot
-              about HTML &amp; CSS!
+              Hello! My name is Priscilla and I'm a new graduate from UC Santa Barbara. 
+              I'm currently seeking full-time opportunities in full-stack and back-end development.
             </p>
 
             <p>
-              Fast-forward to today, and I’ve had the privilege of working at{' '}
-              <a href="https://us.mullenlowe.com/">an advertising agency</a>,{' '}
-              <a href="https://starry.com/">a start-up</a>,{' '}
-              <a href="https://www.apple.com/">a huge corporation</a>, and{' '}
-              <a href="https://scout.camd.northeastern.edu/">a student-led design studio</a>. My
-              main focus these days is building accessible, inclusive products and digital
-              experiences at <a href="https://upstatement.com/">Upstatement</a> for a variety of
-              clients.
+              I'm interested in software engineering because it is a dynamic field that gives me opportunities to learn and grow at every iteration. 
+              My main focus these days is creating passion projects built by gamers, for gamers. I'm currently self-learning various languages and softwares to build QOL bots and applications. 
             </p>
 
             <p>
-              I also recently{' '}
-              <a href="https://www.newline.co/courses/build-a-spotify-connected-app">
-                launched a course
-              </a>{' '}
-              that covers everything you need to build a web app with the Spotify API using Node
-              &amp; React.
+              I enjoy solving unique and challenging problems, and I am eager to communicate with and adapt to new environments.
             </p>
 
             <p>Here are a few technologies I’ve been working with recently:</p>
